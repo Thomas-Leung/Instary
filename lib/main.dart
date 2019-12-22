d4108f035bfb2e509e2df5f68c418498ac09cf21
thomas <thomaselmoleung@gmail.com>
2019-12-22 15:54:18 -0500

    learn REST API and search function

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,151 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 import './viewPage.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-    debugShowCheckedModeBanner: false
-  ));
+  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // controls the text label we use as a search bar
+  final TextEditingController _filter = TextEditingController();
+  final dio = Dio(); // for http requests
+  String _searchText = "";
+  List names = List(); // names we get from API
+  List filteredNames = List(); // names filtered by search text
+  Icon _searchIcon = Icon(Icons.search);
+  Widget _appBarTitle = new Text('Search Example');
+
+  // evaluates whether there is text currently in our search bar, and if so,
+  // appropriately sets our _searchText to whatever that input is so we can filter our list accordingly
+  _HomePageState() {
+    _filter.addListener(() {
+      if (_filter.text.isEmpty) {
+        setState(() {
+          _searchText = "";
+          filteredNames = names;
+        });
+      } else {
+        setState(() {
+          _searchText = _filter.text;
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    this._getNames();
+    super.initState();
+  }
+
+  void _getNames() async {
+    final response = await dio.get('https://swapi.co/api/people');
+    List tempList = new List();
+    for (int i = 0; i < response.data['results'].length; i++) {
+      tempList.add(response.data['results'][i]);
+    }
+
+    setState(() {
+      names = tempList;
+      filteredNames = names;
+    });
+  }
+
+  void _searchPressed() {
+    setState(() {
+      if (this._searchIcon.icon == Icons.search) {
+        this._searchIcon = new Icon(Icons.close);
+        this._appBarTitle = new TextField(
+          controller: _filter,
+          decoration: new InputDecoration(
+              prefixIcon: new Icon(Icons.search), hintText: 'Search...'),
+        );
+      } else {
+        this._searchIcon = new Icon(Icons.search);
+        this._appBarTitle = new Text('Search Example');
+        filteredNames = names;
+        _filter.clear();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      appBar: _buildBar(context),
       body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                  icon: Icon(Icons.favorite, color: Colors.redAccent),
-                  iconSize: 70.0,
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => ViewPage(
-                          instaryTitle: "Title",
-                          instaryPhoto: "assets/images/cat.png",
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.favorite, color: Colors.redAccent),
+                    iconSize: 70.0,
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => ViewPage(
+                            instaryTitle: "Title",
+                            instaryPhoto: "assets/images/cat.png",
+                          ),
                         ),
-                      ),
-                    );
-                  }),
-              Text("Home")
-            ],
+                      );
+                    },
+                  ),
+                  Text("Home"),
+                  Container(
+                    child: _buildList(),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildBar(BuildContext context) {
+    return new AppBar(
+      centerTitle: true,
+      title: _appBarTitle,
+      leading: new IconButton(
+        icon: _searchIcon,
+        onPressed: _searchPressed,
+      ),
+    );
+  }
+
+  Widget _buildList() {
+    if (_searchText != "") {
+      List tempList = new List();
+      for (int i = 0; i < filteredNames.length; i++) {
+        if (filteredNames[i]['name']
+            .toLowerCase()
+            .contains(_searchText.toLowerCase())) {
+          tempList.add(filteredNames[i]);
+        }
+      }
+      filteredNames = tempList;
+    }
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemCount: names == null ? 0 : filteredNames.length,
+      itemBuilder: (BuildContext context, int index) {
+        return new ListTile(
+          title: Text(filteredNames[index]['name']),
+          onTap: () => print(filteredNames[index]['name']),
+        );
+      },
     );
   }
 }

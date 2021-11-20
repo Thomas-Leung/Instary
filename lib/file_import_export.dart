900dffe50f50521f5d6fc303f3dc134aa15af4b5
Thomas Leung <32366586+Thomas-Leung@users.noreply.github.com>
2021-11-20 18:18:15 -0500

    able to save Zip Instary and Images

--- lib/file_import_export.dart
+++ lib/file_import_export.dart
@@ -1,17 +1,14 @@
 import 'dart:io';
 
+import 'package:archive/archive_io.dart';
 import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
 import 'package:intl/intl.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:instary/models/instary.dart';
 
 class FileImportExport {
-  List<Instary> temp = [
-    new Instary("1", DateTime.now(), "Test2", "This is the first content", 1.0,
-        2.0, 3.0, ["asdf", "qwer", "zxcv"], [])
-  ];
-
   // TODO: use file_picker 1.13.3 to get the file path
   void readFile() async {
     File file = File('backup/data.iry');
@@ -26,14 +23,54 @@ class FileImportExport {
   }
 
   void writeBackup() async {
-    var jsonContent = encondeToJson(temp);
-    print(encondeToJson(temp));
+    List jsonContent = _backupInstary();
+
+    // convert json Instary to a File
     final fileName =
         "${DateFormat('yyyy-MM-dd_HH-MM').format(DateTime.now())}.json";
     final directory = await path_provider.getTemporaryDirectory();
     final filePath = path.join(directory.path, fileName);
     File tempFile = await File(filePath).writeAsString(jsonContent.toString());
-    MediaStore().downloadBackup(file: tempFile, name: fileName);
+
+    // Add json File to Zip
+    var encoder = ZipFileEncoder();
+    String zipTempFilePath = path.join(directory.path, "temp.zip");
+    encoder.create(zipTempFilePath);
+    encoder.addFile(tempFile);
+
+    // Add images to Zip
+    final instaryBox = Hive.box('instary');
+    for (int i = 0; i < instaryBox.length; i++) {
+      Instary instary = instaryBox.getAt(i);
+      for (int j = 0; j < instary.imagePaths.length; j++) {
+        File image = new File(instary.imagePaths[j]);
+        encoder.addFile(image);
+      }
+    }
+    encoder.close();
+
+    // Save Zip file to download folder in Android
+    File zipFile = File(zipTempFilePath);
+    String zipFileName =
+        "${DateFormat('yyyy-MM-dd_HH-MM').format(DateTime.now())}.zip";
+    MediaStore().downloadBackup(file: zipFile, name: zipFileName);
+
+    // Delete templorary Directory (Might need this in the future)
+    // Directory dir = await path_provider.getTemporaryDirectory();
+    // dir.deleteSync(recursive: true);
+    // dir.create(); // This will create the temporary directory again. So temporary files will only be deleted
+  }
+
+  List _backupInstary() {
+    List<Instary> temp = [];
+    final instaryBox = Hive.box('instary');
+    // to initialize instaries when start
+    for (int i = 0; i < instaryBox.length; i++) {
+      final instary = instaryBox.getAt(i);
+      temp.add(instary);
+    }
+    print(encondeToJson(temp));
+    return encondeToJson(temp);
   }
 
   List encondeToJson(List<Instary> list) {

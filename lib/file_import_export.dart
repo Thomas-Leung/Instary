f8ffb1239f3dafee84377b9309a51734fc281b5d
Thomas Leung <32366586+Thomas-Leung@users.noreply.github.com>
2021-11-23 22:16:13 -0500

    add logic to import images and json

--- lib/file_import_export.dart
+++ lib/file_import_export.dart
@@ -21,6 +21,7 @@ class FileImportExport {
     if (result != null) {
       File file = File(result.files.single.path!);
       print("Import file location" + file.path);
+      Directory appDir = await path_provider.getApplicationDocumentsDirectory();
       Directory tempDir = await path_provider.getTemporaryDirectory();
       // Read the Zip file from disk.
       final bytes = file.readAsBytesSync();
@@ -31,22 +32,34 @@ class FileImportExport {
       // Extract the contents of the Zip archive to disk.
       for (final file in archive) {
         final filename = file.name;
+        print(file.name);
         if (file.isFile) {
-          final data = file.content as List<int>;
+          // Images insider the Images folder are also treated as file
           if (filename == "instary.json") {
-            // write to file
+            final data = file.content as List<int>;
             File('${tempDir.path}/' + filename)
               ..createSync(recursive: true)
               ..writeAsBytesSync(data);
+            print("Add to temp folder");
+          } else if (filename.toLowerCase().contains("images")) {
+            final data = file.content as List<int>;
+            File(appDir.path + filename) // filename already contains "Image/"
+              ..createSync(recursive: true)
+              ..writeAsBytesSync(data);
+            print("Add to Image folder");
           }
         } else {
-          // In the future, we can just put image and video directory directly.
+          // Not sure when it will be treated as Directory, so far everything are files
           Directory('${tempDir.path}/' + filename).create(recursive: true);
         }
       }
     } else {
       // User canceled the picker
     }
+    // TODO: Create Instary, also we import image path, we should know if user importing from android or iOS
+    // E.g. When export, we can create a metedata.json to stored if the data is exported from Android or iOS
+    // https://stackoverflow.com/questions/45300661/how-to-check-the-device-os-version-from-flutter
+    // At least, platform (Android or iOS) and app version
   }
 
   void writeBackup() async {

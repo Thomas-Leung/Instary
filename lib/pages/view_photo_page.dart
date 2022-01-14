e9518440ff239f6188a7df1bda61dc8b4460b76e
Thomas Leung <32366586+Thomas-Leung@users.noreply.github.com>
2022-01-13 19:02:28 -0500

    able to show miltiple photos in viewPage

--- lib/pages/view_photo_page.dart
+++ lib/pages/view_photo_page.dart
@@ -2,15 +2,15 @@ import 'package:flutter/widgets.dart';
 import 'package:photo_view/photo_view.dart';
 
 class ViewPhotoPage extends StatelessWidget {
-  final imagePath;
-  ViewPhotoPage({this.imagePath});
+  final imageFile;
+  ViewPhotoPage({this.imageFile});
   @override
   Widget build(BuildContext context) {
     return Container(
       child: PhotoView(
         // Reference: https://stackoverflow.com/questions/66561177/the-argument-type-object-cant-be-assigned-to-the-parameter-type-imageprovide
-        imageProvider: imagePath != null
-            ? FileImage(imagePath)
+        imageProvider: imageFile != null
+            ? FileImage(imageFile)
             : AssetImage('assets/images/img_not_found.png') as ImageProvider,
       ),
     );
